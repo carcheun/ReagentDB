29efea7910042b83268c01a6014549ea91be3494
carcheun <carolyncheung@lumatas.com>
2020-09-04 10:33:54 -0700

    move around files

--- web/ReagentDB/settings.py
+++ web/ReagentDB/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +20,14 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 SECRET_KEY = '_1fl$e!6#+&(brk8&qsi8&9itgglq=ti7*yhswk0od^zmg(sc9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = int(os.environ.get("DEBUG", default=0))
 DEBUG = True
 
+#ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 ALLOWED_HOSTS = []
 
 
@@ -37,9 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reagents',         # our reagents app we created
-    'rest_framework',   # needed for our rest_framework
-    'django_extensions',# extra manange.py goodies, especially reset_db for development
+
+    'ReagentDB.apps.reagents',          # our reagents app we created
+    'rest_framework',                   # needed for our rest_framework
+    'django_extensions',                # extra manange.py goodies
 ]
 
 MIDDLEWARE = [
